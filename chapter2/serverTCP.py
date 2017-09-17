54ee38cd3264c55b8b35e416e99614bd2ad1efec
kali <kali_hacker@sina.com>
2017-09-17 20:30:35 +0800

    tcp server

---
+++ chapter2/serverTCP.py
@@ -0,0 +1,29 @@
+from socket import *
+from threading import *
+
+bind_ip = "127.0.0.1"
+bind_port = 6666
+
+ser_sock = socket(AF_INET, SOCK_STREAM)
+
+ser_sock.bind((bind_ip, bind_port))
+
+ser_sock.listen(5)
+
+print("[*] Listening on %s:%d" % (bind_ip, bind_port))
+
+def handle_client(client_socket):
+    '''print the message the clent send'''
+    request = client_socket.recv(1024)
+    print("[*] Received: %s" % request)
+
+    client_socket.send("ACK")
+    client_socket.close()
+
+while True:
+    client, addr = ser_sock.accept()
+    print("[*] Accepted connection from %s:%d" % (addr[0], addr[1]))
+
+    client_handler = Thread(target=handle_client, args=(client,))
+    client_handler.start()
+
--- chapter2/tcpClient2.py
+++ chapter2/tcpClient2.py
@@ -6,6 +6,6 @@ ser_port = 6666
 cli_sock = socket(AF_INET, SOCK_STREAM)
 
 cli_sock.connect((ser_host, ser_port))
-cli_sock.send("GET / HTTP/1.1\r\nHost:baidu.com\r\n\r\n") 
+cli_sock.send("this is from tcp2") 
 response = cli_sock.recv(4096)
 print(response)
